4ae913fcee8e20509c2be62a6f8ae22ddf298550
meresse kyalwe <meressekyalwe@live.com>
2024-02-18 19:13:30 +0300

    initial commit

---
+++ tests/main_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+TEST(TestGroupName, Subtest_1) {
+	ASSERT_TRUE(1 == 1);
+}
+
+TEST(TestGroupName, Subtest_2) {
+	ASSERT_FALSE('b' == 'b');
+	std::cout << "continue test after failure" << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
