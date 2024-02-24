6eaa6d9ce235e4d10f4ce8423c909646cd4b9a4d
meresse kyalwe <meressekyalwe@live.com>
2024-02-24 06:17:13 +0300

    updated for tests

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -5,12 +5,6 @@ TEST(TestGroupName, Subtest_1) {
 	ASSERT_TRUE(1 == 1);
 }
 
-TEST(TestGroupName, Subtest_2) {
-	ASSERT_FALSE('b' == 'b');
-	std::cout << "continue test after failure" << std::endl;
-}
-
-
 int main(int argc, char** argv)
 {
 	testing::InitGoogleTest(&argc, argv);
